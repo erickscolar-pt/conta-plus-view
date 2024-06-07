af881a0b5d2bdff9063a8594dd4b56f8b29677a1
Erick Scolar <erick.scolar@outlook.com>
2024-06-07 12:40:20 -0300

    [main][GA]: [Configuração do Google Analytics]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,16 @@ import Head from "next/head"
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify'
 import { AuthProvider } from "../contexts/AuthContexts";
+import GoogleAnalytics from "@/component/GoogleAnalytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
                 <title>Conta+</title>
-
-
+                <meta name='description' content='Simplifique suas finanças com Conta Plus: gerenciamento inteligente de contas pessoais para um futuro financeiro mais tranquilo e organizado.' />
             </Head>
+            <GoogleAnalytics/>
             <AuthProvider>
                 <Component {...pageProps} />
                 <ToastContainer autoClose={3000} />
