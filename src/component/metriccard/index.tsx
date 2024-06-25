8f0e7475896ce96e678a3c8dc9cf84bf262e0fa5
Erick Scolar <erick.scolar@outlook.com>
2024-06-25 13:36:05 -0300

    fix: tamanho dos textos em metricas dashboard

--- src/component/metriccard/index.tsx
+++ src/component/metriccard/index.tsx
@@ -4,7 +4,7 @@ export default function MetricCard({ title, value, icon: Icon }) {
     <div className="bg-white shadow-md rounded-lg p-6 m-4 text-center">
       <Icon className="text-indigo-600 text-4xl mx-auto mb-4" />
       <h2 className="text-2xl font-semibold text-gray-700">{title}</h2>
-      <p className="text-4xl font-bold text-indigo-600 mt-2">{value}</p>
+      <p className="text-3xl font-bold text-indigo-600 mt-2">{value}</p>
     </div>
   );
 };
